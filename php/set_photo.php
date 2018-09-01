0e699c7a6c90ae30c2897f6cdda8a6cc3d87630b
Unknown <zzwyxl@163.com>
2018-09-01 18:57:25 +0800

    更新上传，可以传多张图片

--- php/set_photo.php
+++ php/set_photo.php
@@ -6,6 +6,8 @@ require("database.php");
 $job_number = $_POST['job_number'];
 $dir = $_POST['url'];
 $change = ["photo",$dir];
+
 set("technician","job_number",$job_number,[$change]);
+add("technician_photo",[['job_number',$job_number],['img',$dir]])
 
 ?>
