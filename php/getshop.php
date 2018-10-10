e459c1629e74b71ce3e35bf0530ed281cb026052
yun_cn <zzwyxl@163.com>
2018-10-10 10:39:49 +0800

    修改内容

--- php/getshop.php
+++ php/getshop.php
@@ -4,7 +4,8 @@ $shop = get("shop");
 if($shop){
     echo json_encode([
         'name'=>$shop[0]['name'],
-        'position'=>$shop[0]['position']
+        'position'=>$shop[0]['position'],
+        'all_shop'=>$shop
     ]);
 }else{
     echo json_encode([
