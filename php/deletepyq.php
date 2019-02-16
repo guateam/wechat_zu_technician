6724100749c9a6fecc5656c19d11e0c5ac327c3d
wilson <thkdragon@cjlu.edu.cn>
2019-02-17 00:39:22 +0800

    修改下删除朋友圈的php代码

--- php/deletepyq.php
+++ php/deletepyq.php
@@ -2,9 +2,7 @@
 require("database.php");
 date_default_timezone_set('PRC');
 $id = $_POST['id'];
-$result = sql_str("delete from friend_circle where ID='$id'");
-if($result){
-    echo json_encode(['status'=>1]);
-    exit();
-}
-echo json_encode(['status'=>0]);
+
+del("friend_circle","ID",$id);
+echo json_encode(['status'=>1]);
+?>
