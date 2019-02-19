19490d01ffb2f397352fd90917284d45a65a5391
yun_cn <zzwyxl@163.com>
2019-02-19 13:38:21 +0800

    登陆弹窗最新的公告，首页增加公告按钮，点击进入查看详细公告

---
+++ php/getnotice.php
@@ -0,0 +1,14 @@
+<?php
+require('database.php');
+$newest = false;
+if(isset($_POST['newest'])){
+    $newest = true;
+}
+$notice = [];
+if($newest)
+    $notice = sql_str("select * from tech_notice order by date desc limit 1");
+else
+    $notice = sql_str("select * from tech_notice order by date desc");
+
+echo json_encode(['status'=>1,'data'=>$notice]);
+
