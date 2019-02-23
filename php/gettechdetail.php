f5e0a25cc8f393c0ddb4a4cbdea1a06e2923c3ba
wilson <thkdragon@cjlu.edu.cn>
2019-02-23 10:33:01 +0800

    技师端，个人详情，入职日期修改

--- php/gettechdetail.php
+++ php/gettechdetail.php
@@ -5,7 +5,10 @@ $tech = get("technician","job_number",$job_number);
 
 if($tech)
 {
-    $tech[0]['entry_date'] = substr($tech[0]['entry_date'],0,10);
+	$entry_date = substr($tech[0]['entry_date'],0,10);
+	$tech[0]['entry_date'] = date("Y-m-d", $entry_date);
+    //$tech[0]['entry_date'] = substr($tech[0]['entry_date'],0,10);
+	
     $tech[0]['birthday'] = substr($tech[0]['birthday'],0,10);
     echo json_encode([
         'status'=>1,
