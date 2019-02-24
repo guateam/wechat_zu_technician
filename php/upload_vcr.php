983b079699302d67408d37b4458a4b1f867a242b
wilson <thkdragon@cjlu.edu.cn>
2019-02-24 08:14:28 +0800

    视频上传先回到以前的版本，限制15秒

--- php/upload_vcr.php
+++ php/upload_vcr.php
@@ -1,8 +1,7 @@
 <?php
 require("database.php");
-$dir = $_SERVER['DOCUMENT_ROOT']."/vcr/ori/";
-$vcr_dir = $_SERVER['DOCUMENT_ROOT']."/vcr/";
-$save_dir = "/vcr/";//保存到数据库的路径
+$dir = $_SERVER['DOCUMENT_ROOT']."/vcr/";
+$save_dir = "/vcr/";
 $img_dir =$_SERVER['DOCUMENT_ROOT']."/photo/";
 
 //判断操作系统
@@ -44,40 +43,24 @@ if ($_FILES["file"]["type"] == "video/mp4" ||  $_FILES["file"]["type"] == "video
         }
         else
         {
-            $tmstmp = date("ymdhis",time());//时间戳
+            $tm = date("ymdhis",time());
             //图片的名字,包含要保存到的路径，用于创建文件和命令行参数
-            $img_name = $img_dir.$rnd_str.$tmstmp;//放到图片文件夹
+            $img_name = $img_dir.$rnd_str.$tm;
             //保存在数据库中的路径格式
-            $sv_img_name = '/photo/'.$rnd_str.$tmstmp.'.jpg';
-            $sv = $save_dir.$rnd_str.$tmstmp.'.'.$extension;//保存到数据库的路径
-            $tm = $dir.$rnd_str.$tmstmp.'.'.$extension;//$rnd_str 随机7位字符串
-			$tm2 = $vcr_dir.$rnd_str.$tmstmp.'.'.$extension;//$rnd_str 随机7位字符串
-			
+            $sv_img_name = '/photo/'.$rnd_str.$tm.'.jpg';
+            $sv = $save_dir.$rnd_str.$tm.'.'.$extension;
+            $tm=$dir.$rnd_str.$tm.'.'.$extension;
 			//保存文件到某文件夹	
-            move_uploaded_file($_FILES["file"]["tmp_name"],$tm );//已经上传成功！
+            move_uploaded_file($_FILES["file"]["tmp_name"],$tm );
             //根据系统获取视频第一帧图片，作为视频封面
             $osname = PHP_OS;
             if(strpos($osname,"Linux")!==false)
 			{
                 $osname = 'Linux';
 				
-				//----------------------------------------------------------------------------------
-				$cmd="/monchickey/ffmpeg/bin/ffmpeg -i ".$tm." -f mjpeg -y -ss 3 -t 1 ".$img_name.".jpg";	//视频封面截图 和服务器安装ffmpeg的路径关联
-				$ffmpeg_output2 = shell_exec($cmd);
-				
-				//---------------------------------------------
-				$myfile = fopen("sdr2.txt", "a+") or die("Unable to open file!");				
-				fwrite($myfile, "video cmd = ".$cmd."   "."\r\n");
-				fwrite($myfile, "ffmpeg_output2 = ".$ffmpeg_output2."   "."\r\n");
-				fclose($myfile);
-				//---------------------------------------------
-				//----------------------------------------------------------------------------------
-				
-				//----------------------------------------------------------------------------------
                 //获取视频时长
 				$cmd2 = "/monchickey/ffmpeg/bin/ffmpeg -i \"$tm\" 2>&1";//和服务器安装ffmpeg的路径关联
                 $ffmpeg_output = shell_exec($cmd2);				
-				
 				//---------------------------------------------
 				$myfile = fopen("sdr2.txt", "a+") or die("Unable to open file!");	
 				fwrite($myfile, "video cmd2 = ".$cmd2."   "."\r\n");
@@ -88,28 +71,23 @@ if ($_FILES["file"]["type"] == "video/mp4" ||  $_FILES["file"]["type"] == "video
                 if( preg_match('/.*Duration: ((\d+)(?:\:)(\d+)(?:\:)(\d+))*/i', $ffmpeg_output, $matches) ) 
 				{
                     //超过10秒则停止上传，并提示时长超出
-                    // if ($matches[1] > "00:00:10" )
-					// {
-                        // unlink($tm);//删除视频文件
-                        // echo json_encode(['state'=>"视频时长不能超过10秒"]);
-                        // exit();
-                    // }
-					
-					 //超过10秒截取前10秒
-					 if ($matches[1] > "00:00:10" )
-					 {
-						$cmd3 = "/monchickey/ffmpeg/bin/ffmpeg  -i $tm -vcodec copy -acodec copy -ss 00:00:00 -to 00:00:10 $tm2";
-						$ffmpeg_output_2 = shell_exec($cmd3);	
-						 
-						//---------------------------------------------
-						$myfile = fopen("sdr2.txt", "a+") or die("Unable to open file!");	
-						fwrite($myfile, "cmd3 = ".$cmd3."   "."\r\n");
-						fwrite($myfile, "ffmpeg_output_2 = ".$ffmpeg_output."   "."\r\n");
-						fclose($myfile);
-						//---------------------------------------------
-					 }
+                    if ($matches[1] > "00:00:15" )
+					{
+                        unlink($tm);
+                        echo json_encode(['state'=>"视频时长不能超过15秒"]);
+                        exit();
+                    }
                 } 
-				//----------------------------------------------------------------------------------
+                
+				$cmd="/monchickey/ffmpeg/bin/ffmpeg -i ".$tm." -f mjpeg -y -ss 3 -t 1 ".$img_name.".jpg";	//和服务器安装ffmpeg的路径关联
+				$ffmpeg_output2 = shell_exec($cmd);
+				
+				//---------------------------------------------
+				$myfile = fopen("sdr2.txt", "a+") or die("Unable to open file!");				
+				fwrite($myfile, "video cmd = ".$cmd."   "."\r\n");
+				fwrite($myfile, "ffmpeg_output2 = ".$ffmpeg_output2."   "."\r\n");
+				fclose($myfile);
+				//---------------------------------------------
             }
 			else if(strpos($osname,"WIN")!==false)
 			{
