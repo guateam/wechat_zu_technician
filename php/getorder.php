7c782c26c36407a7e3917edc878f35a98ba553ef
wilson <thkdragon@cjlu.edu.cn>
2019-02-28 22:38:45 +0800

    把接待和技师分开算工资

--- php/getorder.php
+++ php/getorder.php
@@ -1,113 +1,147 @@
 <?php
-require "database.php";
+	require "database.php";
 
-$job_number = $_POST["job_number"];
+	$job_number = $_POST["job_number"];
 
-//找到对应工号的技师的类别
-$type = sql_str("select type from technician where job_number = '$job_number'");
+	//找到对应工号的技师的类别
+	$type = sql_str("select type from technician where job_number = '$job_number'");
 
-if ($type && count($type) > 0) 
-{
-    $type = $type[0]['type'];
-}
-
-
-$date = $_POST["date"];
-if($date == "")
-{
-    $date = date("Y-m-01 00:00:00",time());
-}
-else
-{
-    $date.=" 00:00:00";
-}
-$date = strtotime($date);
-
-$date2 = "";
-if (!isset($_POST["date2"])) 
-{
-    $date2 = date("Y-m-t 23:59:59",time());
-} 
-else 
-{
-    $date2 = $_POST["date2"]." 23:59:59";
-}
-$date2 = strtotime($date2);
-
-$service_order = get("service_order", "job_number", $job_number);
-if (!$service_order) 
-{
-    $service_order = [];
-}
-
-$consumed_order = [];
-$service_type = get("service_type");
-if (!$service_type) 
-{
-    $service_type = [];
-}
-$dian = 0;
-$pai = 0;
-$last_soid = "";
-$key_info = [];
-//提成
-$ticheng = 0;
-//业绩
-$yeji = 0;
-$all_time = false;
-if ($date == "") 
-{
-    $all_time = true;
-}
-
-foreach ($service_order as $so) 
-{
-    $one_consumed_order = get("consumed_order", "order_id", $so['order_id']);
-    $time = $one_consumed_order[0]['end_time'];
-    if ($all_time) 
+	if ($type && count($type) > 0) 
 	{
-        $date = $time;
-        $date2 = $time;
-    }
-    
-	if ($so['service_type'] == 1 && ($time >= $date && $time <= $date2) && ($one_consumed_order[0]['state'] == 4 || $one_consumed_order[0]['state'] == 5)) 
-	{        
-        $tp = get("service_type","ID",$so['item_id']);
+		$type = (int)$type[0]['type'];
+	}
 
-        if($so['clock_type'] == 1)$pai++;
-        else if($so['clock_type'] == 2)$dian++;
-        $ticheng += $so['ticheng']/100;
-        
-		//$yeji += $one_consumed_order[0]['pay_amount']/100;
 
-        $yeji += $so['price']/100;
-        
-		array_push($key_info, [
-                        "service_name" => $tp[0]['name'],
-                        "room_number" => $so['private_room_number'],
-                        "bonus" => (int) $so['ticheng']/100,
-                        "time" => $one_consumed_order[0]['end_time'],
-                        "order_id" => $so['order_id'],
-                        "income" => $so['price']/100,
-                        "clock_type" => $so['clock_type'],
-        ]);
-
-    }
-    if ($so['order_id'] != $last_soid) 
+	$date = $_POST["date"];
+	if($date == "")
 	{
-        array_push($consumed_order, $one_consumed_order[0]);
-    }
-    $last_soid = $so['order_id'];
-}
+		$date = date("Y-m-01 00:00:00",time());
+	}
+	else
+	{
+		$date.=" 00:00:00";
+	}
+	$date = strtotime($date);
 
-echo json_encode((object) [
-    'total_clock' => $pai + $dian,
-    'dian'=>$dian,
-    'pai'=>$pai,
-    'bonus' => $ticheng,
-    'total_income' => $yeji,
-    'key_info' => $key_info,
-    'type' => $type,
-]);
+	$date2 = "";
+	if (!isset($_POST["date2"])) 
+	{
+		$date2 = date("Y-m-t 23:59:59",time());
+	} 
+	else 
+	{
+		$date2 = $_POST["date2"]." 23:59:59";
+	}
+	$date2 = strtotime($date2);
+
+	if ($type == 1)
+	{
+		$service_order = get("service_order", "job_number", $job_number);
+		if (!$service_order) 
+		{
+			$service_order = [];
+		}
+	}
+	else if ($type == 2)
+	{
+		$service_order = get("service_order", "jd_number", $job_number);
+		if (!$service_order) 
+		{
+			$service_order = [];
+		}
+	}
+
+	$consumed_order = [];
+	$service_type = get("service_type");
+	if (!$service_type) 
+	{
+		$service_type = [];
+	}
+
+	$dian = 0;
+	$pai = 0;
+	$last_soid = "";
+	$key_info = [];
+	//提成
+	$ticheng = 0;
+	//业绩
+	$yeji = 0;
+	$all_time = false;
+	if ($date == "") 
+	{
+		$all_time = true;
+	}
+
+	foreach ($service_order as $so) 
+	{
+		$one_consumed_order = get("consumed_order", "order_id", $so['order_id']);
+		$time = $one_consumed_order[0]['end_time'];
+		if ($all_time) 
+		{
+			$date = $time;
+			$date2 = $time;
+		}
+		
+		if ($so['service_type'] == 1 && ($time >= $date && $time <= $date2) && ($one_consumed_order[0]['state'] == 4 || $one_consumed_order[0]['state'] == 5)) 
+		{        
+			$tp = get("service_type","ID",$so['item_id']);
+
+			if($so['clock_type'] == 1)
+			{
+				$pai++;
+			}
+			else if($so['clock_type'] == 2)
+			{
+				$dian++;
+			}
+
+			if ($type == 1)
+			{
+				//$yeji += $one_consumed_order[0]['pay_amount']/100;//不按照实际付款金额计算
+				$yeji += $so['price'] / 100;//按照设定的项目活动价格来计算
+			
+				$ticheng += $so['ticheng'] / 100;
+
+				array_push($key_info, [
+						"service_name" => $tp[0]['name'],
+						"room_number" => $so['private_room_number'],
+						"bonus" => (int) $so['ticheng']/100,
+						"time" => $one_consumed_order[0]['end_time'],
+						"order_id" => $so['order_id'],
+						"income" => $so['price']/100,
+						"clock_type" => $so['clock_type'],
+				]);
+			}
+			else if ($type == 2)
+			{
+				$ticheng += $so['jd_ticheng'] / 100;
+
+				array_push($key_info, [
+						"service_name" => $tp[0]['name'],
+						"room_number" => $so['private_room_number'],
+						"bonus" => (int) $so['jd_ticheng']/100,
+						"time" => $one_consumed_order[0]['end_time'],
+						"order_id" => $so['order_id'],
+						"income" => $so['price']/100,
+						"clock_type" => $so['clock_type'],
+				]);
+			}
+		}
+		if ($so['order_id'] != $last_soid) //这里是为了防止同一个订单的多个服务项目，而插入多次订单
+		{
+			array_push($consumed_order, $one_consumed_order[0]);
+		}
+		$last_soid = $so['order_id'];
+	}
+
+	echo json_encode((object) [
+		'total_clock' => $pai + $dian,
+		'dian'=>$dian,
+		'pai'=>$pai,
+		'bonus' => $ticheng,
+		'total_income' => $yeji,
+		'key_info' => $key_info,
+		'type' => $type,
+	]);
 
 ?>
--- php/getsalary.php
+++ php/getsalary.php
@@ -2,42 +2,70 @@
 
 function getsalary($id)
 {
-    $service = sql_str("select A.* from service_order A,consumed_order B where A.job_number = '$id'and A.order_id=B.order_id and (B.state=4 or B.state=5)");
-    //获取技师类型
-    // $type = sql_str("select type from technician where job_number='$id'");
-    // if ($type) {
-    //     $type = $type[0]['type'];
-    // }
+	//获取技师类型
+    $type = sql_str("select type from technician where job_number='$id'");
+    if ($type) 
+	{
+        $type = (int)$type[0]['type'];//1为技师，2为接待
+    }
+	
+	if ($type === 1)
+	{	
+		$service = sql_str("select A.* from service_order A,consumed_order B where A.job_number = '$id' and A.order_id=B.order_id and (B.state=4 or B.state=5)");//处于待评价或者评价完成的服务
+	}
+	else if ($type === 2)
+	{	
+		$service = sql_str("select A.* from service_order A,consumed_order B where A.jd_number = '$id' and A.order_id=B.order_id and (B.state=4 or B.state=5)");//处于待评价或者评价完成的服务
+	} 
+	
     $todaysalary = 0;
     $salary = 0;
     foreach ($service as $value) 
 	{
-            $order = get('consumed_order', 'order_id', $value['order_id']);
-            if ($order) 
+		$order = get('consumed_order', 'order_id', $value['order_id']);
+		if ($order) 
+		{
+			if ($value['service_type'] == 1) //1 店内服务 2 酒水饮料
 			{
-                    if ($value['service_type'] == 1) 
+					$day_end = strtotime(date('Y-m-d') . ' 23:59:59');
+					$day_begin = strtotime(date('Y-m-d') . ' 00:00:00');
+					$month_begin = strtotime(date('Y-m') . '-01 00:00:00');
+					$month_end = strtotime(date('Y-m-t') . ' 23:59:59');
+					
+					if ($type == 1)
 					{
-                            $day_end = strtotime(date('Y-m-d') . ' 23:59:59');
-                            $day_begin = strtotime(date('Y-m-d') . ' 00:00:00');
-                            $month_begin = strtotime(date('Y-m') . '-01 00:00:00');
-                            $month_end = strtotime(date('Y-m-t') . ' 23:59:59');
-                            if ($order[0]['end_time'] <= $day_end && $order[0]['end_time'] >= $day_begin) 
-							{
-                                $todaysalary +=$value['ticheng']/100;
-                            }
-                            if ($order[0]['end_time'] <= $month_end && $order[0]['end_time'] >= $month_begin) 
-							{
-                               $salary +=$value['ticheng']/100;
-                            }
-                    }
-            }
+						if ($order[0]['end_time'] <= $day_end && $order[0]['end_time'] >= $day_begin) 
+						{
+							$todaysalary +=$value['ticheng']/100;//技师项目提成
+						}
+						if ($order[0]['end_time'] <= $month_end && $order[0]['end_time'] >= $month_begin) 
+						{
+						   $salary +=$value['ticheng']/100;
+						}
+					}
+					else if ($type == 2)
+					{
+						if ($order[0]['end_time'] <= $day_end && $order[0]['end_time'] >= $day_begin) 
+						{
+							$todaysalary += $value['jd_ticheng']/100;//接待项目提成
+						}
+						if ($order[0]['end_time'] <= $month_end && $order[0]['end_time'] >= $month_begin) 
+						{
+						   $salary += $value['jd_ticheng']/100;
+						}
+					}
+			}
+		}
     }
-    $invite_bonus = get_invite_bonus($id);
-    return ['status' => 1, 'todaysalary' => $todaysalary+$invite_bonus['invite_today'], 'salary' => $salary+$invite_bonus['invite_month']];
+    
+    //$invite_bonus = get_invite_bonus($id);
+    //return ['status' => 1, 'todaysalary' => $todaysalary + $invite_bonus['invite_today'], 'salary' => $salary + $invite_bonus['invite_month']];//今日提成，本月提成
+	
+    return ['status' => 1, 'todaysalary' => $todaysalary, 'salary' => $salary ];//今日提成，本月提成，佣金不算在提成里面
 }
 
 
-function get_invite_bonus($job_number)
+function get_invite_bonus($job_number)//佣金
 {
     $begin_today = date("Y-m-d 00:00:00",time());
     $end_today = date("Y-m-d 23:59:59",time());
@@ -60,6 +88,7 @@ function get_invite_bonus($job_number)
 		{
             //获取下家信息
             $fresh_jbnb = $inv['freshman_job_number'];
+			
             //获取下家支付给本家的钱
             $yeji_today = get_lost($inv['freshman_job_number'], $begin_today, $end_today);
             $yeji_month = get_lost($inv['freshman_job_number'], $begin_month, $end_month);
